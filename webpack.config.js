5ab87ef3972ca8898be9910d14b39d6f4815609a
robert.kratz <robert.kratz@students.uni-mannheim.de>
2024-09-06 17:53:51 +0200

    updated to electron 27+

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
 module.exports = {
   // Configuration options
   output: {
@@ -6,4 +8,18 @@ module.exports = {
   devServer: {
     historyApiFallback: true, // This ensures all paths fall back to index.html
   },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      // Configuration options
+      async: false, // consider setting async to false if you face race conditions
+      typescript: {
+        configFile: "tsconfig.json",
+      },
+      logger: {
+        infrastructure: "console",
+        issues: "console",
+        devServer: true,
+      },
+    }),
+  ],
 };
